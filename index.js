bb0340dba5ca8e1db4fd71266b62ef890af52bdb
Kuzmiankou Anatoli <njkznu@gmail.com>
2018-04-14 00:00:41 +0300

    Improve task

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require('babel-register');
-require('./app.js');
+import appConfig from './config/app';
+import { User, Product } from './models';
+
+console.log(`Project name: ${ appConfig.name }`);
+console.log(`User: ${ new User({ firstName: 'First', lastName: 'Last', email: 'Email@mail' }) }`);
+console.log(`Product: ${ new Product({ name: 'Product' }) }`);
