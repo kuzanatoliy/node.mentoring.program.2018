00f81de60c07b4eaa2b8ed57542bd6f86547c01d
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-04-13 18:24:29 +0300

    Homework created

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "standard",
+  rules: {
+    "semi": ["error", "always"],
+    "comma-dangle": ["error", "always"],
+    "template-curly-spacing": ["error", "always"],
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "never",
+      "asyncArrow": "always"
+    }],
+    "comma-dangle": ["error", "always-multiline"],
+  },
+};
