d0f35d8c914f6126bf1342f4c6fdbfe0ce4c1aa9
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-04-14 09:59:02 +0300

    Create task

---
+++ utils/importer/Importer.js
@@ -0,0 +1,15 @@
+class Importer {
+  constructor() {
+    this.smt = null;
+  }
+
+  import(path) {
+    return null;
+  }
+
+  importSync(path) {
+    return null;
+  }
+}
+
+export default Importer;
