713ce3f647bb9b03c6dd83392282d17c189ea798
Kuzmiankou Anatoli <njkznu@gmail.com>
2018-04-28 07:22:48 +0300

    Add event

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import * as models from './models';
+import { Importer, DirWatcher } from './utils';
 
-/*console.log(`Project name: ${ appConfig.name }`);
-console.log(`User: ${ new User({ firstName: 'First', lastName: 'Last', email: 'Email@mail' }) }`);
-console.log(`Product: ${ new Product({ name: 'Product', description: 'Description for Product' }) }`);*/
+const watcher = DirWatcher.watch('./data', 5000);
 
-import { Importer, DirWotcher } from './utils';
+const importer = new Importer(models, watcher);
 
-const whatcher = DirWotcher.watch('./data/products.csv', 100, true);
-
-const importer = new Importer(models);
-
-whatcher.start();
+watcher.start();
 
 console.log(importer.import('./data/products.csv'));
 console.log(importer.importSync('./data/products.csv'));
--- utils/dir-wotcher/index.js
+++
@@ -1,1 +0,0 @@
-export { default as DirWotcher } from './DirWotcher';
