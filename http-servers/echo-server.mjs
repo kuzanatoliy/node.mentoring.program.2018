1e5ef8d5f07ec24f1e3d39497829160215df0e84
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-06-20 14:45:09 +0300

    Remove http-servers

--- http-servers/echo-server.mjs
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-  req.pipe(res);
-});
-
-server.listen(3040, () => console.log('Start listen on port 3040'));
--- http-servers/plain-text-server.mjs
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World');
-});
-
-server.listen(3030, () => console.log('Start listen on port 3030'));
