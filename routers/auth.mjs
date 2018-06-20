ae83699a1b5fe62f8b61564bb973cbc8b6607ec5
Kuzmiankou Anatoli <njkznu@gmail.com>
2018-06-21 01:11:11 +0300

    Fix change for google

--- routers/auth.mjs
+++ routers/auth.mjs
@@ -1,31 +1,40 @@
 import passport from 'passport';
 import TwitterStrategy from 'passport-twitter';
-import OAuth2Strategy from 'passport-oauth2';
+import GoogleOAuthStrategy from 'passport-google-oauth20';
 import { sendJsonData } from '../utils/response';
 import { signJWT } from '../utils/jwt';
-import OAUTH_CONFIGS from '../configs/oauth';
+// import OAUTH_CONFIGS from '../configs/oauth';
 
-const { TWITTER_OAUTH_CONFIG } = OAUTH_CONFIGS;
+// const { TWITTER_OAUTH_CONFIG } = OAUTH_CONFIGS;
 
-passport.use(new TwitterStrategy(TWITTER_OAUTH_CONFIG, (token, tokenSecret, profile, cb) => {
-  return cb(null, profile);
+// passport.use(new TwitterStrategy(TWITTER_OAUTH_CONFIG, (token, tokenSecret, profile, cb) => {
+//   return cb(null, profile);
+// }));
+
+passport.use(new GoogleOAuthStrategy({
+  clientID: '782564964019-q4dehbr1vd673s1docfderu811r83i6u.apps.googleusercontent.com',
+  clientSecret: 'TPAGM9nBaqr-R2hQ8otxI2NG',
+  callbackURL: "http://localhost:3000/api/auth/login/google/callback",
+}, (accessToken, refreshToken, profile, cb) => {
+  console.log(profile);
+  cb(null, profile);
 }));
-
-passport.use(new OAuth2Strategy(
-  
-), (token, tokenSecret, profile, cb) => {
-
-});
 
 export function setAuthApi(router) {
   router.route('/auth/login')
     .get(loginTreatment);
 
-  router.route('/auth/login/twitter')
+  router.route('/auth/login/google')
+    .get(passport.authenticate('google', { session: false, scope: ['profile', 'email'] }));
+
+  router.route('/auth/login/google/callback')
+    .get(passport.authenticate('google', { session: false, scope: ['profile', 'email'] }), twitterCallbackTreatment);
+
+  /* router.route('/auth/login/twitter')
     .get(passport.authenticate('twitter', { session: false }));
 
   router.route('/auth/login/twitter/callback')
-    .get(passport.authenticate('twitter', { session: false }), twitterCallbackTreatment);
+    .get(passport.authenticate('twitter', { session: false }), twitterCallbackTreatment); */
 }
 
 export function loginTreatment(req, res) {
