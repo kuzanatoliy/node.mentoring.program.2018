99b5509fdb01d2393df2872956d1d6b90005ee43
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-06-21 19:28:17 +0300

    Fix files oauth

--- index.mjs
+++ index.mjs
@@ -1,11 +1,15 @@
-import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import app from './app';
 
 import APP_CONFIGS from './configs/app';
 
 const { NAME, PORT } = APP_CONFIGS;
 
-const server = http.createServer(app);
+const server = https.createServer({
+  key: fs.readFileSync('configs/certs/server.key'),
+  cert: fs.readFileSync('configs/certs/server.crt'),
+}, app);
 
 server.listen(PORT, () => {
   console.log(`App ${ NAME } was started on port: ${ PORT }`);
--- routers/index.mjs
+++ routers/index.mjs
@@ -1,10 +1,2 @@
-import { setAuthApi } from './auth';
-import { setErrorApi } from './error';
-import { setProductsApi } from './products';
-import { setUsersApi } from './users';
-
-const apiSetters = [ setAuthApi, setProductsApi, setUsersApi, setErrorApi ];
-
-export function setApi(router) {
-  apiSetters.forEach(item => item(router));
-}
+export * from './api';
+export * from './passport-auth';
