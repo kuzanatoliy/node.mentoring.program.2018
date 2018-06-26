aab4ab6e314c0cbbc9e74ed0ccb368014962f75e
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-06-26 18:07:38 +0300

    Migrations refactoring

--- bin/migrations/201805280701-products.js
+++ bin/migrations/201805280701-products.js
@@ -1,30 +1,31 @@
 module.exports = {
-  up: (queryInterface, DataTypes) => queryInterface.createTable('Products', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: DataTypes.TEXT,
-    shortDescription: DataTypes.TEXT,
-    price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-  }),
+  up: (queryInterface, DataTypes) =>
+    queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: DataTypes.TEXT,
+      shortDescription: DataTypes.TEXT,
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    }),
 
   down: queryInterface => queryInterface.dropTable('Products'),
 };
--- bin/migrations/201806252241-cities.js
+++ bin/migrations/201806252241-cities.js
@@ -1,36 +1,37 @@
 module.exports = {
-  up: (queryInterface, DataTypes) => queryInterface.createTable('Cities', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    name: {
-      allowNull: false,
-      type: DataTypes.STRING,
-    },
-    country: {
-      allowNull: false,
-      type: DataTypes.STRING,
-    },
-    capital: {
-      allowNull: false,
-      type: DataTypes.BOOLEAN,
-    },
-    location: {
-      allowNull: false,
-      type: DataTypes.STRING,
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-  }),
+  up: (queryInterface, DataTypes) =>
+    queryInterface.createTable('Cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      country: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      capital: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+      },
+      location: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    }),
 
   down: queryInterface => queryInterface.dropTable('Cities'),
 };
